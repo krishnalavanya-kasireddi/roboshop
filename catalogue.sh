7b18b08bdb95de17dfaf3969ca58a9425c5b0f3a
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-02-03 15:59:24 -0500

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -75,8 +75,8 @@ dnf install mongodb-mongosh -y &>>$LOGS_FILE
 INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -le 0 ]; then
-    #mongosh --host $MONGODB_HOST </app/db/master-data.js
-    #VALIDATE $? "Loading products"
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
 else
     echo -e "Products already loaded ... $Y SKIPPING $N"
 fi
